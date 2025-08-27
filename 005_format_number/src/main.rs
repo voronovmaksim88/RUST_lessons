b6717978e8d9038e48fe0d9c2ca9f21178990223
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-08-27 09:31:40 +0700

    прописал типы подробено

---
+++ 005_format_number/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let number: u8 = 0b1000_0001; // Двоичный формат
+    println!("number = {}", number);
+
+    let number: u8 = 0o10; // Восьмеричный формат
+    println!("number = {}", number);
+
+    let number: u8 = 0x10; // Шестнадцатеричный формат
+    println!("number = {}", number);
+}
