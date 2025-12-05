a9be3df0a9ee2a8ff33e388b4072956fc08527d6
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-12-05 16:20:07 +0700

    Add boolean comparison demo in main

--- 009_conditional_expressions/src/main.rs
+++ 009_conditional_expressions/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = 7;
+    let result: bool;
+    result = x == y; // result = false - x не равно y
+    println!("x={}, y={}, выражение x==y имеет значение {}", x, y, result);
 }
