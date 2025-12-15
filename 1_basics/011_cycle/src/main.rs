ed46aabe9e6a8c2efac8b7c3cae73fb3cea592d0
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-12-15 21:35:46 +0700

    Update main.rs

--- 1_basics/011_cycle/src/main.rs
+++ 1_basics/011_cycle/src/main.rs
@@ -1,39 +1,46 @@
 fn main() {
-    println!("Hello, world!");
-
+    println!("Loop example!");
     // Цикл, который ничего не возвращает
     let mut n = 1;
     loop {
         println!("n = {}", n);
         n = n + 1;
-        if n == 10 {
+        if n == 6 {
             break;
         }
     }
+    println!("");
 
+    println!("Loop and break example!");
     // Цикл, который возвращает значение
     let mut m = 1;
     let result = loop {
         println!("m = {}", m);
         m = m + 1;
-        if m == 10 {
+        if m == 6 {
             break m * 2;
         }
     };
     println!("Результат цикла: {}", result);
+    println!("");
 
+    println!("While example!");
     // Цикл while
     let mut n = 1;
-    while n < 10 {
+    while n < 6 {
         println!("n = {}", n);
         n = n + 1;
     }
+    println!("");
 
+    println!("For example!");
     // Цикл for
     for num in 1..6 {
         println!("num = {}", num);
     }
+    println!("");
 
+    println!("Nested loop example!");
     // Вложенный цикл
     let mut i = 1;
     let mut j = 1;
@@ -46,7 +53,9 @@ fn main() {
         i = i + 1;
         j = 1;
     }
+    println!("");
 
+    println!("Loop with label example!");
     // Цикл с меткой
     let mut count = 0;
     'outer: loop {
@@ -65,6 +74,7 @@ fn main() {
         count += 1;
     }
     println!("Final count = {count}");
+    println!("");
 
     println!("Конец программы");
 }
