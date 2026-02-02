f138c926b9fe66d9b0427f95f1ea7c1cfc8ada23
voronovmaksim88 <voronovmaksim88@gmail.com>
2026-02-02 17:03:15 +0700

    иправил поддержку цветов

--- book/guess_the_number/src/main.rs
+++ book/guess_the_number/src/main.rs
@@ -4,7 +4,8 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Я закадал число от 1 до 100. Угадай его за 7 попыток чтобы выиграть!");
+    let _ = colored::control::set_virtual_terminal(true);
+    println!("Я загадал число от 1 до 100. Угадай его за 7 попыток чтобы выиграть!");
     let secret_number = rand::rng().random_range(1..=100);
     let mut attempts = 0;
 
