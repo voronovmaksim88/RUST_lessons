703bf42d79eb038958cfca04109f478b8fb32e86
voronovmaksim88 <voronovmaksim88@gmail.com>
2026-02-02 16:56:31 +0700

    напромтил игру угадай число.

---
+++ book/guess_the_number/src/main.rs
@@ -0,0 +1,52 @@
+use colored::Colorize;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Я закадал число от 1 до 100. Угадай его за 7 попыток чтобы выиграть!");
+    let secret_number = rand::rng().random_range(1..=100);
+    let mut attempts = 0;
+
+    loop {
+        println!("Попытка #{}", attempts + 1);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Ошибка чтения ввода");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        attempts += 1;
+
+        if attempts >= 7 && guess != secret_number {
+            println!("{}", "ты проиграл".red());
+            println!("Загаданное число было: {}", secret_number);
+            break;
+        }
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком мало!"),
+            Ordering::Greater => println!("Слишком много!"),
+            Ordering::Equal => {
+                println!(
+                    "{}",
+                    format!(
+                        "Поздравляю! Ты угадал число {} за {} попыток!",
+                        secret_number, attempts
+                    )
+                    .green()
+                );
+                break;
+            }
+        }
+    }
+
+    println!("Нажми Enter, чтобы завершить программу...");
+    let mut exit_input = String::new();
+    let _ = io::stdin().read_line(&mut exit_input);
+}
