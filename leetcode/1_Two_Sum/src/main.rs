74fa546f7c332dbd05143f1d5240bf2cb9f0871a
voronovmaksim88 <voronovmaksim88@gmail.com>
2026-02-03 15:19:20 +0700

    пробую решить задачу литкода

---
+++ leetcode/1_Two_Sum/src/main.rs
@@ -0,0 +1,18 @@
+struct Solution;
+
+fn main() {
+    println!("{:?}", Solution::two_sum(vec![2, 7, 11, 15], 9));
+}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for _ in 0..nums.len() {}
+        let my_vec: Vec<i32> = vec![1, 2];
+        println!("target {:?}", target);
+        return my_vec;
+    }
+}
+
+// для запуска
+// rustc main.rs
+// .\main.exe
