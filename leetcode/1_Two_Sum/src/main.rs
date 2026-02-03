01c4c0c9de2db92e70c2dec199063d8fdbed3d6a
voronovmaksim88 <voronovmaksim88@gmail.com>
2026-02-03 15:34:01 +0700

    скопировал себе супер быстрое решение

--- leetcode/1_Two_Sum/src/main.rs
+++ leetcode/1_Two_Sum/src/main.rs
@@ -13,12 +13,34 @@ impl Solution {
                 }
             }
         }
-        let my_vec: Vec<i32> = vec![1, 2];
-        println!("target {:?}", target);
-        return my_vec;
+        return vec![];
     }
 }
 
 // для запуска
 // rustc main.rs
 // .\main.exe
+
+// мне пока непонятное, но очень быстрое о скорости решение
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+//         let mut snums:HashMap<&i32,usize> = HashMap::new();
+//         for (i,elem) in nums.iter().enumerate() {
+//             snums.insert(elem, i);
+//         }
+
+//         for (i,elem) in nums.iter().enumerate() {
+//             match snums.get(&(target - elem)) {
+//                 Some(&first_index) => {
+//                     if first_index != i {
+//                         return vec![first_index as i32, i as i32]
+//                         }
+//                     },
+//                 None => (),
+//             }
+//         }
+//     return vec![-1];
+//     }
+// }
