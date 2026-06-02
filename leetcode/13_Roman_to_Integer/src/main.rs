592e2b9aa4cba1d559e1486a39da6ca3a10fd731
voronovmaksim88 <voronovmaksim88@gmail.com>
2026-06-02 23:34:06 +0700

    позорно решил задачу

---
+++ leetcode/13_Roman_to_Integer/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let solution = Solution::roman_to_int("IV".to_string());
+    println!("итог {}", solution);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut n = 0;
+        let mut prev_l: char = ' ';
+        for l in s.chars().rev() {
+            match l {
+                'I' => {
+                    if prev_l == 'V' || prev_l == 'X' {
+                        n -= 1;
+                    } else {
+                        n += 1;
+                    }
+                }
+                'V' => n += 5,
+                'X' => {
+                    if prev_l == 'L' || prev_l == 'C' {
+                        n -= 10;
+                    } else {
+                        n += 10;
+                    }
+                }
+                'L' => n += 50,
+                'C' => {
+                    if prev_l == 'D' || prev_l == 'M' {
+                        n -= 100;
+                    } else {
+                        n += 100;
+                    }
+                }
+                'D' => n += 500,
+                'M' => n += 1000,
+                _ => {}
+            }
+            println!("{}", n);
+            prev_l = l;
+        }
+        n
+    }
+}
